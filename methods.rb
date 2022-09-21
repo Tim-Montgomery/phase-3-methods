2c579e3318d027b2751b03973dc58bbe9b120d68
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-09-21 09:56:33 -0400

    assignment complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+# function greetProgrammer() {
+#     console.log("Hello, programmer!");
+#   }
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name) 
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer") 
+  puts "Hello, #{name}!"
+end
+
+def add(a, b)
+  return (a + b)
+end
+
+def halve(num)
+  if num.class != Integer
+    return nil
+  end
+
+  return num / 2
+
+end
